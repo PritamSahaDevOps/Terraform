670a7ab5c1bea6ad0e11acc22b5fce69b1031634
PritamSahaDevOps <2025pritamsaha@gmail.com>
2025-02-26 22:05:45 +0530

    Update var.sh

--- var.sh
+++ var.sh
@@ -1,8 +1,8 @@
-$env:ARM_CLIENT_ID="06288bc3-1c36-4bb7-b8fc-e55cd67c9059"
-$env:ARM_CLIENT_SECRET="z5W8Q~Hx8P5P8FpDqtkX0FPJt8.elWD1n6lLqaZu"
-$env:ARM_SUBSCRIPTION_ID="2a86af15-0804-4cf9-b548-2faff1df0d48"
-$env:ARM_TENANT_ID="c128c998-2325-41d7-8955-5ece59854250"
+$env:ARM_CLIENT_ID="0000000000000000000000000000000"
+$env:ARM_CLIENT_SECRET="000000000000aaaaaxcccccccccc"
+$env:ARM_SUBSCRIPTION_ID="000000000000000000000000000000"
+$env:ARM_TENANT_ID="00000000000000000000000000000000"
 
-#[System.Environment]::SetEnvironmentVariable('ARM_SUBSCRIPTION_ID', '2a86af15-0804-4cf9-b548-2faff1df0d48', [System.EnvironmentVariableTarget]::User)
+#[System.Environment]::SetEnvironmentVariable('ARM_SUBSCRIPTION_ID', '00000000000000000000000000000', [System.EnvironmentVariableTarget]::User)
 
-#az ad sp create-for-rbac -n az-demo --role="Contributor" --scopes="/subscriptions/2a86af15-0804-4cf9-b548-2faff1df0d48"
+#az ad sp create-for-rbac -n az-demo --role="Contributor" --scopes="/subscriptions/$Sunscriptionid"
